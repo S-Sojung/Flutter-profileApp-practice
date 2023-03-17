dcdd6f125f1ff38fdf80d6ca924c22eb3d78f1d4
sojung <sojung528@gmail.com>
2023-03-17 17:53:57 +0900

    tab-bar modify

--- lib/main.dart
+++ lib/main.dart
@@ -36,16 +36,26 @@ class ProfilePage extends StatelessWidget {
       //Refactor Extract Flutter Widget 으로 만들어서 다른데서 찾을 수 있도록 해줌
       endDrawer: ProfileDrawer(),
       appBar: _appBar(), //얘는 이 파일 안에서만 접근 할 수 있도록 해줌
-      body: Column(
-          children: [
-            SizedBox(height: 20,),
-            ProfileHeader(),
-            SizedBox(height: 20,),
-            ProfileCountInfo(),
-            SizedBox(height: 20,),
-            ProfileButtons(),
-            Expanded(child: ProfileTab())
-          ]
+      // 기본은 Column 이었음 헤더에 고정
+      // 만약 List view 가 되면 바디의 크기가 제약이 없어진다. Expanded를 하면 터져버림.
+      body: NestedScrollView(  //리스트 안의 리스트가 다 확장되도록 함
+        //ListView 처럼 작동하는데.. List 안에 List가 있을 경우 사용.
+          body: ProfileTab(), //크기를 알 수 없는 List : 원래는 동적으로 다 크키 계산 하도록 해야함...
+          headerSliverBuilder: (context, innerBoxIsScrolled) {
+            return [
+              SliverList( // 크기를 알 수 있는 List
+                delegate: SliverChildListDelegate([
+                  SizedBox(height: 20,),
+                  ProfileHeader(),
+                  SizedBox(height: 20,),
+                  ProfileCountInfo(),
+                  SizedBox(height: 20,),
+                  ProfileButtons(),
+                  //암튼 ListView에서 높이 제한이 없는 자식을 가질 수 없다.
+                ])
+              )
+            ]; //SliverList 찢어진 조각 리스트
+          },
       ),
     );
   }
